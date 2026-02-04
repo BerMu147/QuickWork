e7c43f1144391fab46a5c2f7a146e49c5417053d
Berin Mujcinovic <berin.mujcinovic@edu.fit.ba>
2026-02-04 23:38:14 +0100

    Initial Fresh commit

---
+++ QuickWork/QuickWork.WebAPI/Controllers/GenderController.cs
@@ -0,0 +1,28 @@
+using ManiFest.Model.Requests;
+using ManiFest.Model.Responses;
+using ManiFest.Model.SearchObjects;
+using ManiFest.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManiFest.WebAPI.Controllers
+{
+    public class GenderController : BaseCRUDController<GenderResponse, GenderSearchObject, GenderUpsertRequest, GenderUpsertRequest>
+    {
+        public GenderController(IGenderService service) : base(service)
+        {
+        }
+
+        [AllowAnonymous]
+        public override async Task<PagedResult<GenderResponse>> Get([FromQuery] GenderSearchObject? search = null)
+        {
+            return await base.Get(search);
+        }
+
+        [AllowAnonymous]
+        public override async Task<GenderResponse?> GetById(int id)
+        {
+            return await base.GetById(id);
+        }
+    }
+}
